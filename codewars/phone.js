828265f81b41214f934466fc4c9da546b09ce8bf
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-05 10:18:50 +0530

    Create phone.js

---
+++ codewars/phone.js
@@ -0,0 +1,10 @@
+function createPhoneNumber(numbers){
+  var format="(xxx) xxx-xxxx";
+  for (let i =0;i<numbers.length;i++)
+    {
+     format=format.replace('x',numbers[i]);
+     }
+  return format
+}
+
+//output "(123) 456-7890"
